25e413548660d97a100aa9a7c92bacaa7adef45e
User name <supremeshrestha4@gmail.com>
2024-02-21 21:38:26 -0500

    Commiting tests donw so far

---
+++ src/test/java/org/example/testngtests/Firsttest.java
@@ -0,0 +1,57 @@
+package org.example.testngtests;
+
+import org.testng.annotations.*;
+
+public class Firsttest {
+
+    @BeforeSuite
+    public void beforesuite(){
+        System.out.println("beforesuite");
+    }
+
+    @BeforeClass
+    public void beforeclass(){
+        System.out.println("beforclass");
+    }
+
+    @BeforeTest
+    public void beforetest(){
+        System.out.println("beforetest");
+    }
+
+    @BeforeMethod
+    public void beforemethod(){
+        System.out.println("beforemethod");
+    }
+
+    @Test(priority = 2, invocationCount = 2)
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test(priority = 1, enabled = false)
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @AfterTest
+    public void aftertest(){
+        System.out.println("aftertest");
+    }
+
+    @AfterMethod
+    public void aftermethod(){
+        System.out.println("aftermethod");
+    }
+
+    @AfterClass
+    public void afterclass(){
+        System.out.println("afterclass");
+    }
+
+    @AfterSuite
+    public void aftersuite(){
+        System.out.println("beforesuite");
+    }
+
+}
